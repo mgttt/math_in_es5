ea3b5cbc7709cfe9ed999d609209edca2516cca7
WANJO CHAN <wanjochan@users.noreply.github.com>
2017-05-24 18:10:00 +0800

    Update math_norm_es5.js

--- math_norm_es5.js
+++ math_norm_es5.js
@@ -75,5 +75,20 @@ if(!Math.norm) Math.norm=function(_mean,_standardDeviation){
 			var absPercentile = _zTable[""+zRow][zColIndex];
 			return zScore < 0 ? 1 - absPercentile : absPercentile;
 		};
+		/* @ref https://www.johndcook.com/blog/cpp_phi/ */
+		this.cdf2=function(x){
+			var a1 =  0.254829592;
+			var a2 = -0.284496736;
+			var a3 =  1.421413741;
+			var a4 = -1.453152027;
+			var a5 =  1.061405429;
+			var p  =  0.3275911;
+			x = _abs(x)/_sqrt(2.0);
+			// A&S formula 7.1.26
+			var t = 1.0/(1.0 + p*x);
+			var y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*_exp(-x*x);
+			return 0.5*(1.0 + y*(x<0?-1:1));
+		};
+
 	})(_mean,_standardDeviation);
 };
